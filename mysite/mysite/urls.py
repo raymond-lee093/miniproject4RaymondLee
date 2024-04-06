2e8939ef03dfb49708a93b632779a61aab23824a
Raymond Lee <raymondlee093@gmail.com>
2024-04-06 17:06:38 -0500

    Created homepage.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url='polls/')),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]

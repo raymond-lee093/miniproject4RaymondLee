e5cbce573f2b14e94d109ef1e44e36d7669a3c03
Raymond Lee <raymondlee093@gmail.com>
2024-04-06 10:14:47 -0500

    Modified the Question model display and features of admin page.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -9,8 +9,9 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ["question_text", "pub_date"]
-
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),

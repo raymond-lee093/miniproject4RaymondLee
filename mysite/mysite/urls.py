d5e908120650b3c657d427a290064269b19d6532
Raymond Lee <raymondlee093@gmail.com>
2024-04-06 19:36:34 -0500

    Modified homepage to be login page.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url='polls/')),
+    path("", RedirectView.as_view(url='accounts/login')),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),

e64adbe7dac690e6d2cb5fa932fce7d1b1c92ba0
Raymond Lee <raymondlee093@gmail.com>
2024-04-09 18:26:28 -0500

    Modified root url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
+
 urlpatterns = [
-    path("", RedirectView.as_view(url='accounts/login')),
+    path("", RedirectView.as_view(url='polls/home')),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
